b6139855a089d3c700979f3e4cb7815052d0301c
lokiiiofficial19 <lokiiiofficial1337@gmail.com>
2025-12-20 12:00:54 +0530

    Created portfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,30 @@
 import React from "react";
-import Projects from "./Projects";
 import profileImg from "../assets/IMG_20251129_121652.jpg";
-
 import "./Hero.css";
 
 export default function Hero() {
   return (
     <section className="hero">
       <div className="hero-text">
-        <h4>LOKESHKUMAR MANIKANDAN</h4>
-        <p>“Hi, I’m Lokesh Kumar Manikandan, a front-end developer. I create responsive websites using HTML, CSS, JavaScript, and React.</p>
-        <p>This is my portfolio website. At the top, you can see my name and navigation menu which links to my projects, skills, about, and contact sections.</p>
-        <p>Here in the main section, you see my name, a short description of my skills, and buttons like See Projects to check out my work, and Contact Me to get in touch.</p>
-        <p>I’ve designed it with a modern look – dark hero section, clean white header, and interactive buttons to make navigation simple and attractive.</p>
-        <p>Later, I’ve added sections to showcase my projects, skills, and a contact form so potential employers or clients can learn more about me and reach out easily.”</p>
+        <h1>
+          Hi, I’m <span>Lokesh Kumar</span> 👋
+        </h1>
+
+        <h2>Frontend Developer</h2>
+
+        <p>
+          I build modern, responsive, and user-friendly web applications using
+          HTML, CSS, JavaScript, and React.
+        </p>
+
         <div className="hero-buttons">
-          <a href="#projects" className="btn">See Projects</a>
-          <a href="#contact" className="btn btn-outline">Contact Me</a>
+          <a href="#projects" className="btn primary">See Projects</a>
+          <a href="#contact" className="btn secondary">Contact Me</a>
         </div>
       </div>
+
       <div className="hero-image">
-            <img src={profileImg} alt="My photo" />
+        <img src={profileImg} alt="Lokesh Kumar" />
       </div>
     </section>
   );
